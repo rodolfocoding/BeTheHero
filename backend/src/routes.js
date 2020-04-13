5891a8986a69262054bf8e701eb224241372ed09
rodolfocoding <rodolfocoding@gmail.com>
2020-04-13 20:30:07 -0300

    Consumindo API no mobile

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,26 +1,24 @@
-const express = require('express');
-const OngController = require('./controllers/OngController');
-const IncidentsController = require('./controllers/IncidentsControlle');
-const ProfilleController = require('./controllers/ProfilleController');
-const SessionController = require('./controllers/SessionController');
-const connection =  require('./database/connection');
+const express = require("express");
+const OngController = require("./controllers/OngController");
+const IncidentsController = require("./controllers/IncidentsControlle");
+const ProfilleController = require("./controllers/ProfilleController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
+routes.post("/sessions", SessionController.create);
 
-
-//CHAMA O MÉTODO "create" lá da controller da OngController.js 
-routes.post('/ongs', OngController.create)
+//CHAMA O MÉTODO "create" lá da controller da OngController.js
+routes.post("/ongs", OngController.create);
 //CHAMA O MÉTODO "index" lá da controller da OngController.js. SERÁ USADA APENAS PELO OS DESENVOLVEDORES, PARA CONFERÊNCIA DE DADOS
-routes.get('/ongs', OngController.index)
-//CHAMA O MÉTODO "create" lá da controller da IncidentsController.js 
-routes.post('/incidents', IncidentsController.create);
+routes.get("/ongs", OngController.index);
+//CHAMA O MÉTODO "create" lá da controller da IncidentsController.js
+routes.post("/incidents", IncidentsController.create);
 //CHAMA O MÉTODO "index" lá da controller da IncidentsController.js.
-routes.get('/incidents', IncidentsController.index);
+routes.get("/incidents", IncidentsController.index);
 //CHAMA O MÉTODO "delete" LÁ DA CONTROLLER IncidentsController.js
-routes.delete('/incidents/:id', IncidentsController.delete);
-//CHAMA O MÉTODO "index" lá da controller da ProfilleController.js. 
-routes.get('/profille', ProfilleController.index);
+routes.delete("/incidents/:id", IncidentsController.delete);
+//CHAMA O MÉTODO "index" lá da controller da ProfilleController.js.
+routes.get("/profille", ProfilleController.index);
 
 module.exports = routes;
--- mobile/src/pages/Incident/index.js
+++ mobile/src/pages/Incident/index.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
 import logoImg from "../../assets/logo.png";
 import styles from "./styles";
+import api from "../../services/api";
 
-export default function Incident() {
+export default function Incidents() {
+  const [incidents, setIncidents] = useState([]);
   const navigation = useNavigation();
 
   function navigateToDetail() {
     navigation.navigate("Detail");
   }
+
+  async function loadIncidents() {
+    const response = await api.get("incidents");
+
+    setIncidents(response.data);
+  }
+
+  useEffect(() => {
+    loadIncidents();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -28,19 +40,24 @@ export default function Incident() {
 
       <FlatList
         style={styles.incidentList}
-        data={[1, 2, 3, 4, 5]}
-        keyExtractor={(incident) => String(incident)}
+        data={incidents}
+        keyExtractor={(incident) => String(incident.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={() => (
+        renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
-            <Text style={styles.incidentValue}>APAD</Text>
+            <Text style={styles.incidentValue}>{incident.name}</Text>
 
             <Text style={styles.incidentProperty}>CASO:</Text>
-            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+            <Text style={styles.incidentValue}>{incident.title}</Text>
 
             <Text style={styles.incidentProperty}>VALOR:</Text>
-            <Text style={styles.incidentValue}>R$ 120,00</Text>
+            <Text style={styles.incidentValue}>
+              {Intl.NumberFormat("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              }).format(incident.value)}
+            </Text>
 
             <TouchableOpacity
               style={styles.detailsButton}
